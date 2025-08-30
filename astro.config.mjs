6246e1e7163cbadc36f60081b7b4cb89fe9c41e5
plastirecuperados.salado <plastirecuperados.salado@gmail.com>
2025-08-30 13:31:26 +0000

    chore: Force new Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import firebase from "astro-firebase";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [firebase()]
+});
+
+// Trigger new deployment
